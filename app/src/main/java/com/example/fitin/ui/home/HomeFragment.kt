e970845baf54c3eb5002c39db13691bd65641ce2
samarth2varshney <115732604+samarth2varshney@users.noreply.github.com>
2024-08-31 23:28:54 +0530

    problem still exists

--- app/src/main/java/com/example/fitin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/fitin/ui/home/HomeFragment.kt
@@ -1,20 +1,23 @@
 package com.example.fitin.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.fitin.R
 import com.example.fitin.databinding.FragmentHomeBinding
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,11 +31,37 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        setHasOptionsMenu(true)
+
+        findNavController().navigate(R.id.action_navigation_home_to_rankings)
+
+
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_app_bar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.navigation_rankings -> {
+                Log.i("samarth","chala")
+                findNavController().navigate(R.id.action_navigation_home_to_rankings)
+                true
+            }
+            R.id.navigation_feed -> {
+                findNavController().navigate(R.id.action_navigation_home_to_feed)
+                true
+            }
+            R.id.navigation_plans -> {
+                Log.i("samarth","chala")
+                findNavController().navigate(R.id.action_navigation_home_to_plans)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onDestroyView() {
