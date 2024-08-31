724cc59ad9abeef9fa32588d2f3b4387b992ba04
samarth2varshney <115732604+samarth2varshney@users.noreply.github.com>
2024-08-31 18:31:22 +0530

    added feed fragment

---
+++ app/src/main/java/com/example/fitin/ui/feed/Feed.kt
@@ -0,0 +1,29 @@
+package com.example.fitin.ui.feed
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.fitin.databinding.FragmentFeedBinding
+
+class Feed : Fragment() {
+
+    private var _binding: FragmentFeedBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentFeedBinding.inflate(inflater,container,false)
+        val root = binding.root
+
+        val feedViewModel = ViewModelProvider(this)[FeedViewModel::class.java]
+
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/fitin/ui/feed/FeedViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.fitin.ui.feed
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FeedViewModel : ViewModel() {
+
+//    // Define a MutableLiveData list
+//    private val _feedList = MutableLiveData<List<FeedItem>>()
+//
+//    // Expose the list as LiveData
+//    val feedList: LiveData<List<FeedItem>> get() = _feedList
+//
+//    // Function to set/update the list
+//    fun setFeedList(newList: List<FeedItem>) {
+//        _feedList.value = newList
+//    }
+//
+//    // Optionally, you can provide a method to update the list
+//    fun addFeedItem(item: FeedItem) {
+//        _feedList.value = _feedList.value?.toMutableList()?.apply {
+//            add(item)
+//        }
+//    }
+
+}
